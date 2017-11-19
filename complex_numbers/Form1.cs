85bc7d4bafd74e569002296e0f677ea2ad0642cd
Andrew <a.solomencev@yandex.ru>
2017-11-20 00:00:13 +0300

    Добавил операции с комплексными числами и их вывод
    Взял регулярку с stackoverflow, чуть переделал, так как там учитывалась только положительная мнимая часть

--- complex_numbers/Form1.cs
+++ complex_numbers/Form1.cs
@@ -4,8 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -20,10 +22,10 @@ namespace complex_numbers
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            draw_axis();
         }
 
-        private void show_complex(int re, int im){
+        private void show_complex(float re, float im){
             Graphics gr = this.CreateGraphics();
             float x_centre = (this.Width - 200) / 2, y_centre = this.Height / 2;
             Pen point = new Pen(Color.Black, 4);
@@ -79,7 +81,7 @@ namespace complex_numbers
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e) {
-            draw_axis();
+            //draw_axis();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -87,6 +89,106 @@ namespace complex_numbers
             Graphics gr = this.CreateGraphics();
             gr.Clear(this.BackColor);
             draw_axis();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // немного из stackoverflow, писал на скорую руку, потом запилю класс complex
+            String inp1, inp2;
+            float re1, re2, im1, im2, re,im;
+            inp1 = textBox1.Text;
+            inp2 = textBox2.Text;
+            inp1 = inp1.Replace(',', '.');
+            inp2 = inp2.Replace(',', '.');
+            string regexPattern =
+            @"([-+]?\d+\.?\d*|[-+]?\d*\.?\d+)" +
+            @"\s*" +
+            @"[-+]" +
+            @"\s*" +
+            @"([-+]?\d+\.?\d*|[-+]?\d*\.?\d+)" +
+            @"i";
+            Regex regex = new Regex(regexPattern);
+            if (inp1 != "" && inp2 != "")
+            {
+                Match match1 = regex.Match(inp1);
+                Match match2 = regex.Match(inp2);
+                // первое число
+                re1 = float.Parse(match1.Groups[1].Value, CultureInfo.InvariantCulture);
+                im1 = float.Parse(match1.Groups[2].Value, CultureInfo.InvariantCulture);
+                // второе число 
+                re2 = float.Parse(match2.Groups[1].Value, CultureInfo.InvariantCulture);
+                im2 = float.Parse(match2.Groups[2].Value, CultureInfo.InvariantCulture);
+
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        re = re1 + re2;
+                        im = im1 + im2;
+                        if (im > 0)
+                            textBox3.Text = re.ToString() + "+" + im.ToString() + "i";
+                        else if (im < 0)
+                            textBox3.Text = re.ToString() + im.ToString() + "i";
+                        else if (im == 0)
+                            textBox3.Text = re.ToString() + "+" + im.ToString() + "i";
+                        break;
+                    case 1:
+                        re = re1 - re2;
+                        im = im1 - im2;
+                        if (im > 0)
+                            textBox3.Text = re.ToString() + "+" + im.ToString() + "i";
+                        else if (im < 0)
+                            textBox3.Text = re.ToString() + im.ToString() + "i";
+                        else if (im == 0)
+                            textBox3.Text = re.ToString() + "+" + im.ToString() + "i";
+                        break;
+                    case 2:
+                        re = re1 * re2 - im1 * im2;
+                        im = im1 * re2 + re1 * im2;
+                        if (im > 0)
+                            textBox3.Text = re.ToString() + "+" + im.ToString() + "i";
+                        else if (im < 0)
+                            textBox3.Text = re.ToString() + im.ToString() + "i";
+                        else if (im == 0)
+                            textBox3.Text = re.ToString() + "+" + im.ToString() + "i";
+                        break;
+                    case 3:
+                        re = (re1 * re2 + im1 * im2) / (re2 * re2 + im2 * im2);
+                        im = (im1 * re2 - re1 * im2) / (re2 * re2 + im2 * im2);
+                        if (im > 0)
+                            textBox3.Text = re.ToString() + "+" + im.ToString() + "i";
+                        else if (im < 0)
+                            textBox3.Text = re.ToString() + im.ToString() + "i";
+                        else if (im == 0)
+                            textBox3.Text = re.ToString() + "+" + im.ToString() + "i";
+                        break;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String inp = textBox3.Text;
+            inp = inp.Replace(',', '.');
+            float re, im;
+            string regexPattern =
+            @"([-+]?\d+\.?\d*|[-+]?\d*\.?\d+)" +
+            @"\s*" +
+            @"[-+]" +
+            @"\s*" +
+            @"([-+]?\d+\.?\d*|[-+]?\d*\.?\d+)" +
+            @"i";
+            Regex regex = new Regex(regexPattern);
+            if (inp != ""){
+                Match match = regex.Match(inp);
+                re = float.Parse(match.Groups[1].Value, CultureInfo.InvariantCulture);
+                im = float.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
+                show_complex(re, im);
+            }
         }
     }
 }
