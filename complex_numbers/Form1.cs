1840e8e8bcfbcfd945a7e30e9a01782bb3902276
Andrew <a.solomencev@yandex.ru>
2017-11-19 19:14:11 +0300

    Доделал отрисовку осей
    Перенес отрисовку и подпись осей в  отдельную функцию

--- complex_numbers/Form1.cs
+++ complex_numbers/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,12 +23,25 @@ namespace complex_numbers
             
         }
 
-        private void Form1_Paint(object sender, PaintEventArgs e) {
-            Graphics gr = e.Graphics;
-            Pen p = new Pen(Color.Red, 3);
-            gr.DrawLine(p, (this.Width-200)/2, 0, (this.Width - 200) / 2, this.Height);
-            gr.DrawLine(p, 0, this.Height/2, this.Width-200, this.Height/2);
+        private void draw_axis()
+        {
+            // отрисовка осей и их подпись
+            Graphics gr = this.CreateGraphics();
+            Pen axis = new Pen(Color.Red, 4);
+            axis.StartCap = LineCap.ArrowAnchor;
+            Font font = new Font("Tahoma", 16);
+            SolidBrush brush = new SolidBrush(Color.Black);
+            // сами оси
+            gr.DrawLine(axis, (this.Width - 200) / 2, 0, (this.Width - 200) / 2, this.Height);
+            gr.DrawLine(axis, this.Width - 200, this.Height / 2, 0, this.Height / 2);
+            // надписи около осей
+            gr.DrawString("Re", font, brush, (this.Width-200)/2 + 10, 10);
+            gr.DrawString("Im", font, brush, (this.Width-200) - 30, this.Height/2 + 10);
             gr.Dispose();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e) {
+            draw_axis();
         }
     }
 }
